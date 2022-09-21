f1c37270d479a12722ab65228ad90eb2cdb7b83e
gerboles <gerboles2.718@gmail.com>
2022-09-22 01:19:14 +0200

    Succesful load of texture

--- game.c
+++ game.c
@@ -2,20 +2,40 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
 #include <glad/glad.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
 
 static int resolution_width = 512;
-static int resolution_height = 448;
+static int resolution_height = 488;
 static int canvas_width = 1024;
 static int canvas_height = 896;
 
 const char *vertex_shader_source = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
+	"layout (location = 0) in vec3 aPos;\n"
+	"layout (location = 1) in vec3 aColor;\n"
+	"layout (location = 2) in vec2 aTexCoord;\n"
+	"out vec3 ourColor;\n"
+	"out vec2 TexCoord;\n"
+	"void main()\n"
+	"{\n"
+	"gl_Position = vec4(aPos, 1.0);\n"
+	"ourColor = aColor;\n"
+	"TexCoord = aTexCoord;\n"
+	"}";
+
+const char *fragment_shader_source = "#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"in vec3 ourColor;\n"
+	"in vec2 TexCoord;\n"
+	"uniform sampler2D ourTexture;\n"
+	"void main()\n"
+	"{\n"
+	"FragColor = texture(ourTexture, TexCoord);\n"
+	"}";
 
 int main (int argc, char **argv) {
 
@@ -31,14 +51,23 @@ int main (int argc, char **argv) {
 	int game_input_x = 0;
 	int game_input_y = 0;
 
-	// OPENGL CONTEXT CREATION
-	glViewport(0, 0, resolution_width, resolution_height);
+	// GAME AUDIO
+	SDL_Init(SDL_INIT_AUDIO);
+	Mix_Music *music;
+	Mix_Chunk *sound;
+	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+	music = Mix_LoadMUS("wav/low.wav");
+	sound = Mix_LoadWAV("wav/sound1.wav");
 
+	// OPENGL CONTEXT CREATION
+	glViewport(0, 0, canvas_width, canvas_height);
+
+	//	X____, Y____, Z___, R___, G___, B___, U___, V___,
 	float vertex_input[] = {
-		+0.5, +0.5, 0, 0, 0,
-		+0.5, -0.5, 0, 0, 1,
-		-0.5, -0.5, 0, 1, 1,
-		-0.5, +0.5, 0, 1, 0
+		+0.5f, +1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 2.0f, 1.0f,
+		+0.5f, -1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 2.0f, 0.0f,
+		-0.5f, -1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f,
+		-0.5f, +1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f
 	};
 
 	int indices [] = {
@@ -46,11 +75,48 @@ int main (int argc, char **argv) {
 		1, 2, 3
 	};
 
-	// VERTEX BUFFER OBJECT
-	int vertex_buffer_object;
+	int vertex_buffer_object, vertex_array_object, element_buffer_object;
+	glGenVertexArrays(1, &vertex_array_object);
 	glGenBuffers(1, &vertex_buffer_object);
+	glGenBuffers(1, &element_buffer_object);
+	
+	glBindVertexArray(vertex_array_object);
+	
 	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_input), vertex_input, GL_STATIC_DRAW);
+	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer_object);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+    	
+	// texture coord attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	int texture;
+	stbi_set_flip_vertically_on_load(true);
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	// set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	// load image, create texture and generate mipmaps
+	int width, height, nrChannels;
+	char *data = stbi_load("bmp/m7a_path_wall2.png", &width, &height, &nrChannels, 0);
+	printf("The image has a width of %d, a height of %d and %d channels.\n", width, height, nrChannels);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	stbi_image_free(data);
 	
 	// VERTEX SHADER
 	int vertex_shader;
@@ -58,16 +124,30 @@ int main (int argc, char **argv) {
 	glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
 	glCompileShader(vertex_shader);
 
+	// FRAGMENT SHADER
+	int fragment_shader;
+	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+	glCompileShader(fragment_shader);
+
+	// SHADER PROGRAM
+	int shader_program;
+	shader_program = glCreateProgram();
+	glAttachShader(shader_program, vertex_shader);
+	glAttachShader(shader_program, fragment_shader);
+	glLinkProgram(shader_program);
+	glUseProgram(shader_program);
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
+	
 	bool running = true;
 	SDL_Event event;
 
-///////////////
-// GAME LOOP //
-	while(running)
-	{
-		while(SDL_PollEvent(&event))
-		{
-			////////// INPUT LOOP //////////
+	////////// GAME LOOP //////////
+	while(running) {
+		//Mix_PlayMusic(music, -1);
+		////////// INPUT LOOP //////////
+		while(SDL_PollEvent(&event)) {
 			switch (event.type) {
 				case SDL_KEYDOWN:
 					switch (event.key.keysym.sym) {
@@ -82,6 +162,7 @@ int main (int argc, char **argv) {
 						case SDLK_UP:
 							printf("KEY UP\n");
 							game_input_y = +1;
+							Mix_PlayChannel(-1, sound, 0);
 							break;
 						case SDLK_DOWN:
 							game_input_x = -1;
@@ -117,20 +198,40 @@ int main (int argc, char **argv) {
 							break;
 					}
 			}
-			////////// END INPUT LOOP //////////
-
 			if(event.type == SDL_QUIT) running = false;
-
 		}
-		glClearColor(0, 0, 255, 255);
-		glClear(GL_COLOR_BUFFER_BIT);
+		////////// END OF INPUT LOOP //////////
+		
+		////////// RENDER LOOP //////////
+		glClearColor(0, 255, 255, 255);
+		glClear(GL_COLOR_BUFFER_BIT);	
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glUseProgram(shader_program);
+		glBindVertexArray(vertex_array_object);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		SDL_GL_SwapWindow(window);
-		//SDL_RenderClear(renderer);
-		//SDL_RenderPresent(renderer);
+		///////// END RENDER LOOP //////////
+
+		////////// AUDIO LOOP //////////
+		////////// END OF AUDIO LOOP //////////
 	}
 
-	//SDL_DestroyRenderer(renderer);
+	// Destroy audios and sounds
+	Mix_FreeMusic(music);
+	music = NULL;
+	Mix_FreeChunk(sound);
+	sound = NULL;
+
+	// Destroy OpenGL functionalities
+	glDeleteVertexArrays(1, &vertex_array_object);
+	glDeleteBuffers(1, &vertex_buffer_object);
+	glDeleteProgram(shader_program);
+	
+	// Destroy window
 	SDL_DestroyWindow(window);
+	
+	// Quit SDL subsystems
+	Mix_Quit();
 	SDL_Quit();
 	return 0;
 }
